d3f49cfaad2fb2eb38b548b8b0724cfb40189ac5
desnos7 <beugremicaeldesnos@gmail.com>
2022-06-10 16:19:28 +0000

    api

--- api_fecth/script.js
+++ api_fecth/script.js
@@ -22,8 +22,6 @@ function verifie(){
     }
 }
 
-
-
 function store() {
    
    
--- api_fecth/search.js
+++ api_fecth/search.js
@@ -1,40 +1,23 @@
 let contener = document.querySelector('.main')
 let icon = document.querySelector('.icon')
-let search = document.querySelector('#search')
+let searchs = document.querySelector('#search')
 let form = document.querySelector('#form')
-console.log(search);
+console.log(searchs);
 let imgUrl = "https://image.tmdb.org/t/p/w500"
-let 
+ 
 
-let searchValue
-form.addEventListener('submit',(e)=>{
-    e.preventDefault();
-    searchValue = search.value  
-    console.log(searchValue);
-
-if(searchValue && searchValue !==''){
-  
-    searchValue=''
-} else {
-    window.location.reload();
-}
-  })
+async function gene_search(e){
+    e.preventDefault()
+    let dat= await fetch(`https://api.themoviedb.org/3/search/movie?query=${searchs.value}&api_key=7843f8d22a43911f15301ef8d76338ae&language=fr-FR&sort_by=popularity.desc&include_adult=false&include_video=false`)
+    dat= await dat.json();
+    generat_card(dat.results)
+ 
+     }
 
 
 
-async function gene_search(){
-  
-   let dat= await fetch(`https://api.themoviedb.org/3/search/movie?query=${searchValue}&api_key=7843f8d22a43911f15301ef8d76338ae&language=fr-FR&sort_by=popularity.desc&include_adult=false&include_video=false`)
-   dat= await dat.json();
-  
-//    contenu(dat.results)
-   
-  
-
-    }
-    gene_search() 
-
-    function contenu(conte){
+form.addEventListener('submit', gene_search)
+function contenu(conte){
         contener.innerHTML='';
         return (`
         <div class='box' id=${conte.id} onclick="generate_information(event)">
@@ -50,13 +33,13 @@ async function gene_search(){
     
     
     
-    // function generat_card(array){
-    //     contener.innerHTML==''
+function generat_card(array){
+        contener.innerHTML=''
     
-    //     for (let i=0 ;i<array.length; i++){
-    //         contener.innerHTML+=contenu(array[i])
+        for (let i=0 ;i<array.length; i++){
+            contener.innerHTML+=contenu(array[i])
                  
-    //     }
+        }
     
-    // }
+    }
     
